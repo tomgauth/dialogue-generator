de7dcda10cce67c6f530e65fa43692a7c284c553
tom gauthier <tomgauthier@MacBook-Pro-de-tom.local>
2023-09-29 11:27:35 +0200

    added feature for generating flashcards

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import genanki
 import requests
 from pydub import AudioSegment
 import os
@@ -16,6 +17,37 @@ HEADERS = {
     "Authorization": "Bearer YOUR_API_TOKEN"
 }
 
+
+model_id = 1607392319  # This can be any unique number. You can also randomize this as per the documentation.
+dialogue_model = genanki.Model(
+  model_id,
+  'Dialogue Comprehension Model',
+  fields=[
+    {'name': 'Audio'},
+    {'name': 'Text'},
+  ],
+  templates=[
+    {
+      'name': 'Card 1',
+      'qfmt': '{{Audio}}',
+      'afmt': '{{FrontSide}}<hr id="answer">{{Text}}',
+    },
+  ])
+
+deck_id = 2059400110  # Again, this can be any unique number or randomized.
+dialogue_deck = genanki.Deck(deck_id, 'DialogueDeck')
+
+def add_dialogue_to_deck(audio_file, dialogue_text):
+    global dialogue_deck
+    note = genanki.Note(
+      model=dialogue_model,
+      fields=[f'[sound:{audio_file}]', dialogue_text])
+    dialogue_deck.add_note(note)
+
+def generate_anki_package(output_file, media_files=[]):
+    my_package = genanki.Package(dialogue_deck)
+    my_package.media_files = media_files
+    my_package.write_to_file(output_file)
 
 def generate_audio(character, gender, line):
     """
@@ -55,6 +87,8 @@ def main():
     uploaded_file = st.file_uploader("Upload your dialogue text file:", type="txt")
     pasted_content = st.text_area("Or paste your dialogue content here:")
 
+    
+
     if uploaded_file:
         content = uploaded_file.read().decode()
     elif pasted_content:
@@ -68,6 +102,8 @@ def main():
         # Split and parse the content
         lines = content.split("\n")[1:]  # Skipping the title
         audio_segments = []
+        media_files = []  # list to store media files for the Anki package
+
 
         for line in lines:
             if not line.strip():  # Skip empty lines
@@ -82,6 +118,14 @@ def main():
             audio_segment = generate_audio(character, gender, text)
             audio_segments.append(audio_segment)
 
+            # Save the individual audio segment with unique name
+            audio_filename = f"{character}_{text[:10]}.mp3"  # taking first 10 chars of text for simplicity
+            audio_segment.export(audio_filename, format="mp3")
+            media_files.append(audio_filename)
+
+            # Add to Anki deck
+            add_dialogue_to_deck(audio_filename, text)            
+
         # Combine audio segments
         combined_audio = combine_audio_segments(audio_segments)
 
@@ -92,6 +136,13 @@ def main():
         # Allow user to listen to the audio
         st.audio(combined_filename, format="audio/mp3")
         st.markdown(f"[Download the combined audio]({combined_filename})")
+
+        # Add button for generating Anki flashcards
+        if st.button("Generate Anki Flashcards"):
+            output_file = "dialogues_deck.apkg"
+            generate_anki_package(output_file, media_files)
+            st.success(f"Flashcards generated! [Download the Anki Package]({output_file})")
+
 
 
 if __name__ == "__main__":
